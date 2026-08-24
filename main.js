f66b7da366c82d2002444e068beca10ddd52f661
nunez25 <jmmorenonunez2024@hccci.edu.ph>
2026-08-24 17:47:02 +0800

    Task 2: add promise chain demo

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ out.textContent = "Waiting 2 seconds...";
 await wait(2000);
 out.textContent = "Done!";
 });
+
+const fakeApi = (label, ms) => wait(ms).then(() => `[${label}]`);
+document.getElementById("btn-chain").addEventListener("click", () => {
+const out = document.getElementById("out-chain");
+out.textContent = "Starting chain...\n";
+fakeApi("Login", 500)
+.then((r) => {
+out.textContent += r + "\n";
+return fakeApi("Fetch Profile", 700);
+})
+.then((r) => {
+out.textContent += r + "\n";
+return fakeApi("Fetch Posts", 500);
+})
+.then((r) => {
+out.textContent += r + "\n";
+out.textContent += "All done!";
+})
+.catch((err) => {
+out.textContent += "Error: " + err.message;
+});
+});
